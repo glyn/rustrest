b7f67fcc990e33c20a6d3aa9a600f2f18a2adff0
Glyn Normington <gnormington@pivotal.io>
2017-04-11 13:45:36 +0100

    Working example for http:

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+extern crate hyper;
+
+use std::env;
+use std::io::Read;
+use hyper::Client;
+use hyper::client::response::Response;
+
 fn main() {
-    println!("Hello, world!");
+    let url = match env::args().nth(1) {
+        Some(url) => url,
+        None => {
+            println!("Usage: client <url>");
+            return;
+        }
+    };
+
+    let url = url.parse::<hyper::Url>().unwrap();
+    if url.scheme() != "http" {
+        println!("This example only works with 'http' URLs.");
+        return;
+    }
+
+    let client = Client::new();
+
+    let result = client.get(url).send().and_then(|res| {
+        println!("Response: {}", res.status);
+        println!("Headers: \n{}", res.headers);
+        read_to_string(res)
+    }).map(|s| {
+        println!("Response body: {}", s);
+        println!("\n\nDone.");
+    });
+    match result {
+        Ok(_) => println!("\n\nDone."),
+        Err(e) => println!("\n\nFailed {:?}", e),
+    }
+}
+
+fn read_to_string(mut r: Response) -> hyper::Result<String> {
+    let mut s = String::new();
+    try!(r.read_to_string(&mut s));
+    Ok(s)
 }
